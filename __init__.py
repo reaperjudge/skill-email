50fd7e48ee3f727495eee882ace2838e69a1dbca
reaperjudge <38620064+reaperjudge@users.noreply.github.com>
2018-05-08 20:51:35 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ class HelloWorldSkill(MycroftSkill):
     def handle_greetings(self, message):
         dialog = self.dialog_renderer.render("greetings", None)
         # Sending a command to mycroft, speak Greetings Dialog
-        self.send_email("Message From Your Picroft", dialog + <object width="400" height="400" data="~/putinmeme02.jpg"></object>)
+        self.send_email("Message From Your Picroft", dialog + <object width="400" height="400" data="putinmeme02.jpg"></object>)
         self.speak(dialog)
     def stop(self):
         pass
